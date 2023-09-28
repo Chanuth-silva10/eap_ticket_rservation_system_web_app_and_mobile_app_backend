1ab590e83bc92d237260c0dbd99e517b182959c1
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-09-28 20:15:31 +0530

    Finished user backend service

--- Controllers/UserManagerController.cs
+++ Controllers/UserManagerController.cs
@@ -19,14 +19,74 @@ namespace TicketReservationManager.Controllers
 
         }
 
-        // Create a new user
+        // Get All Users
+        [HttpGet]
+        public async Task<List<UserManagerModel>> GetUsers()
+        {
+            _loggerInfo.LogInformation("UserManagerController => GetUsers()");
+            return await _userManagerService.GetAllUsersAsync();
+
+        }
+
+        // Get User using Id
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<UserManagerModel>> GetById(string id)
+        {
+            _loggerInfo.LogInformation("UserManagerController - GetById()");
+            var user = await _userManagerService.GetUserByIdAsync(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // Create user
         [HttpPost]
         public async Task<IActionResult> Post(UserManagerModel createUser)
         {
             _loggerInfo.LogInformation("UserManagerController => Post()");
-            await _userManagerService.CreateAsync(createUser);
+            await _userManagerService.CreateUserAsync(createUser);
 
-            return CreatedAtAction(new { id = createUser.Id }, createUser);
+            return CreatedAtAction(nameof(GetUsers), new { id = createUser.Id }, createUser);
+        }
+
+        // Update user
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, UserManagerModel updatedUser)
+        {
+            _loggerInfo.LogInformation("UserController - Update()");
+            var User = await _userManagerService.GetUserByIdAsync(id);
+            Console. WriteLine(User);
+            if (User is null)
+            {
+                return NotFound();
+            }
+
+            updatedUser.Id = User.Id;
+
+            await _userManagerService.UpdateUserAsync(id, updatedUser);
+
+            return NoContent();
+        }
+
+        // Delete User
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            _loggerInfo.LogInformation("UserManagerController => Delete()");
+            var User = await _userManagerService.GetUserByIdAsync(id);
+
+            if (User is null)
+            {
+                return NotFound();
+            }
+
+            await _userManagerService.DeleteUserAsync(id);
+
+            return NoContent();
         }
     }
 }
--- Services/UserManagerService.cs
+++ Services/UserManagerService.cs
@@ -18,19 +18,52 @@ namespace TicketReservationManager.Services
 
         }
 
-        //Get All Users
-        public async Task<List<UserManagerModel>> GetAllAsync()
+        // Get All Users inside the Admin side only
+        public async Task<List<UserManagerModel>> GetAllUsersAsync()
         {
-            _loggerInfo.LogInformation("UserService - GetAllAsync()");
+            _loggerInfo.LogInformation("UserManagerService using GetAllUsersAsync()");
             return await _userManagerCollection.Find(_ => true).ToListAsync();
 
         }
+        
 
-        //create new user
-        public async Task CreateAsync(UserManagerModel createUser)
+        // create new user inside the admin side only
+        public async Task CreateUserAsync(UserManagerModel createUser)
         {
-            _loggerInfo.LogInformation("Start UserManagerService using CreateAsync()");
+            _loggerInfo.LogInformation("UserManagerService using CreateAsync()");
             await _userManagerCollection.InsertOneAsync(createUser);
+
+        }
+
+        // Get User By Id inside the admin side only
+        public async Task<UserManagerModel?> GetUserByIdAsync(String id)
+        {
+            _loggerInfo.LogInformation("Getting user id UserManagerService using GetByIdAsync()");
+            return await _userManagerCollection.Find(USER => USER.Id == id).FirstOrDefaultAsync();
+
+        }
+
+        // Verify User By NIC and Password inside the admin side only
+        public async Task<UserManagerModel?> VerifyUserByNICAndPasswordAsync(String nic, String password)
+        {
+            _loggerInfo.LogInformation("Verify User UserManagerService using VerifyUserByNICAndPasswordAsync()");
+            return await _userManagerCollection.Find(USER => USER.NIC == nic && USER.Password == password).FirstOrDefaultAsync();
+
+        }
+
+        // Update user inside the admin side only
+        public async Task UpdateUserAsync(string id, UserManagerModel updatedUser)
+        {
+            _loggerInfo.LogInformation("Update User UserManagerService using UpdateUserAsync()");
+            await _userManagerCollection.ReplaceOneAsync(USER => USER.Id == id, updatedUser);
+
+        }
+
+        // Delete User inside the admin side only
+        public async Task DeleteUserAsync(string id)
+        {
+            _loggerInfo.LogInformation("Deleting User Manager Service using DeleteUserAsync()");
+            await _userManagerCollection.DeleteOneAsync(USER => USER.Id == id);
 
         }
     }
