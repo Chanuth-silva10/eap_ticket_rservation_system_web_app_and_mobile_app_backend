2ebb0065a42fbd5d3f30bee592c7f07ae5dce3cf
Chanuth <80547770+Chanuth10@users.noreply.github.com>
2023-09-28 17:25:15 +0530

    setup logger and set user collection for the initilizre

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
+using TicketReservationManager.Models;
+using TicketReservationManager.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = LoggerFactory.Create(config =>
+{
+    config.AddConsole();
+}).CreateLogger("TicketReservation");
+
+builder.Services.Configure<DBConnection>(
+    builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<UserManagerService>();
 
 builder.Services.AddControllers();
 
@@ -6,6 +18,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.Logger.LogInformation("Ticket Reservation Manager Started");
 
 if (app.Environment.IsDevelopment())
 {
